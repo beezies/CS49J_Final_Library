6b861194f64bc8fe39fca0f24ebcb497d1f6b230
beezies <briannaxrogers@yahoo.com>
2023-04-28 18:18:22 -0700

    added rough user classes

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 public class LoginPage {
 
 	public static void main(String[] args) {
+		
 		makeLoginFrame();
 	}
 
--- src/User.java
+++ src/User.java
@@ -3,29 +3,41 @@ abstract class User {
 
 	private String userName;
 	private String password;
-	
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
-	
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
+	abstract String getName();
 
 }
 
 class Member extends User {
 
-	public Member(String userName, String password) {
+	private String firstName;
+	private String lastName;
+
+	public Member(String firstName, String lastName, String userName, String password) {
+		this.firstName = firstName;
+		this.lastName = lastName;
 		setUserName(userName);
 		setPassword(password);
-		
+
+	}
+
+	@Override
+	String getName() {
+		return firstName + lastName;
 	}
 
 }
 
 class AdminUser extends User {
-	
+
 	private final String userName = "ADMIN";
 	private final String password = "ADMIN";
 
@@ -33,5 +45,10 @@ class AdminUser extends User {
 		setUserName(userName);
 		setPassword(password);
 	}
-	
+
+	@Override
+	String getName() {
+		return "Admin User";
+	}
+
 }
