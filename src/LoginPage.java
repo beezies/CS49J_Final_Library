48dddba09187a418d6beb0f6ab1acccd838a0057
beezies <briannaxrogers@yahoo.com>
2023-05-04 03:07:19 -0700

    revamp login code

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -1,45 +1,39 @@
 import java.awt.*;
 import java.awt.event.*;
 import java.util.InputMismatchException;
-
 import javax.swing.*;
 
+@SuppressWarnings("serial")
 public class LoginPage extends JFrame implements ActionListener {
 
-	public static void main(String[] args) {
+	private JPanel panel = new JPanel();
+	private JLabel welcomeLabel = new JLabel("    Welcome to the Library!");
+	private JLabel usrLabel = new JLabel("Enter your username:");
+	private JLabel passLabel = new JLabel("Enter your password:");
+	private JTextField usrField = new JTextField();
+	private JTextField passField = new JTextField();
+	private JButton loginButton = new JButton("Login");
+	private JLabel newUserLabel = new JLabel(
+			"<html><h1><font size =\"4\"><u>New User? Click Here!</u></font></h1></html>");
+	private JLabel errorLabel = new JLabel(
+			"<html><font size='4' color='red'> Your username or password is incorrect. </font></html>");
 
-		LoginPage lp = new LoginPage();
-		lp.makeLoginFrame();
-	}
+	public LoginPage() {
 
-	public void makeLoginFrame() {
+		setTitle("Login Page");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(275, 300);
 
-		JFrame frame = new JFrame("Login Page");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(275, 300);
-
-		JPanel panel = new JPanel();
 		panel.setBackground(Color.pink);
 		panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
-		frame.getContentPane().add(panel);
 
-		JLabel welcomeLabel = new JLabel("    Welcome to the Library!");
 		welcomeLabel.setFont(new Font("serif", Font.PLAIN, 20));
-		JLabel usrLabel = new JLabel("Enter your username:");
-		JLabel passLabel = new JLabel("Enter your password:");
-
-		JTextField usrField = new JTextField();
 		usrField.setMaximumSize(new Dimension(500, usrField.getPreferredSize().height));
-		JTextField passField = new JTextField();
 		passField.setMaximumSize(new Dimension(500, passField.getPreferredSize().height));
-
-		JButton loginButton = new JButton("Login");
-
-		JLabel newUserLabel = new JLabel("<html><h1><font size =\"4\"><u>New User? Click Here!</u></font></h1></html>");
-		
-		JLabel errorLabel = new JLabel("<html><font size='4' color='red'> Your username or password is incorrect. </font></html>");
+		loginButton.addActionListener(this);
 		errorLabel.setBackground(Color.red);
 
+		getContentPane().add(panel);
 		panel.add(new JLabel("                 "));
 		panel.add(welcomeLabel);
 		panel.add(new JLabel("                 "));
@@ -51,46 +45,10 @@ public class LoginPage extends JFrame implements ActionListener {
 		panel.add(loginButton);
 		panel.add(newUserLabel);
 
-		loginButton.addMouseListener(new MouseListener() {
-
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				System.out.println("Check credentials, then sign in if in file/admin");
-				System.out.println("Username: " + usrField.getText());
-
-				try {
-					User user = ValidateUser.validateUser(usrField.getText(), passField.getText());
-				} catch (InputMismatchException i) {
-					System.out.println("die");
-					panel.add(errorLabel);
-					frame.setVisible(true);
-				}
-
-			}
-
-			@Override
-			public void mousePressed(MouseEvent e) {
-			}
-
-			@Override
-			public void mouseReleased(MouseEvent e) {
-			}
-
-			@Override
-			public void mouseEntered(MouseEvent e) {
-			}
-
-			@Override
-			public void mouseExited(MouseEvent e) {
-			}
-
-		});
-
 		newUserLabel.addMouseListener(new MouseListener() {
 
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				System.out.println("Open new user page");
 				SignUpPage f = new SignUpPage();
 			}
 
@@ -112,13 +70,30 @@ public class LoginPage extends JFrame implements ActionListener {
 
 		});
 
-		frame.setVisible(true);
-
+		setVisible(true);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+
+		if (e.getSource() == loginButton) {
+			try {
+				
+				User user = ValidateUser.validateUser(usrField.getText(), passField.getText());
+
+			} catch (InputMismatchException i) {
+				
+				System.out.println("die");
+				panel.add(errorLabel);
+				setVisible(true);
+				
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+
+		LoginPage lp = new LoginPage();
 
 	}
 
