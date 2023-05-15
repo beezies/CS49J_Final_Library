58fc16fd387db258d3b52b7e52548569cc557640
beezies <briannaxrogers@yahoo.com>
2023-05-15 11:05:47 -0700

    added interface

--- src/User.java
+++ src/User.java
@@ -1,21 +1,13 @@
-
 import java.awt.Desktop;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.StandardOpenOption;
-import java.time.LocalDate;
 
-import org.json.JSONObject;
-import org.json.JSONArray;
-import org.apache.commons.io.IOUtils;
-
-abstract class User implements Comparable<User> {
+abstract class User implements Comparable<User>, CanOpenFile {
 
 	public static void main(String[] args) {
+//		Member n = new Member("bee", "via", "bri", "rog");
 		Member n = new Member("bee", "via");
-		n.checkout("amity", "luz");
+		n.returnBook("h", "h");
+		n.returnBook("L", "l");
 
 	}
 
@@ -36,7 +28,8 @@ abstract class User implements Comparable<User> {
 
 	abstract String getPassword();
 
-	protected void openBook(String pdfName) {
+	@Override
+	public void openBook(String pdfName) {
 		if (Desktop.isDesktopSupported()) {
 			try {
 				File myFile = new File(pdfName + ".pdf");
@@ -47,225 +40,8 @@ abstract class User implements Comparable<User> {
 	}
 }
 
-class Member extends User {
+interface CanOpenFile {
 
-	private String firstName;
-	private String lastName;
-	JSONObject memberJSON;
-	JSONArray booksJSON;
-
-	// New account
-	public Member(String userName, String password, String firstName, String lastName) throws IllegalArgumentException {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		setUserName(userName);
-		setPassword(password);
-
-		addToFile();
-	}
-
-	// Quick constructor for login 
-	public Member(String userName, String password) {
-		setUserName(userName);
-		setPassword(password);
-		getMemberJSON();
-	}
-
-	private void addToFile() throws IllegalArgumentException {
-		
-		memberJSON = new JSONObject();
-		booksJSON = new JSONArray();
-
-		try {
-			InputStream is = new FileInputStream(UserHandler.getFileName());
-			String memberFileText = IOUtils.toString(is, "UTF-8");
-			JSONObject membersJSON = new JSONObject(memberFileText);
-			JSONArray membersArray = membersJSON.getJSONArray("members");
-
-			for (int i = 0; i < membersArray.length(); i++) {
-				JSONObject member = membersArray.getJSONObject(i);
-				String uname = member.getString("username");
-				if (uname.equals(userName)) {
-					throw new IllegalArgumentException();
-				}
-			}
-
-			memberJSON.put("username", userName);
-			memberJSON.put("password", password);
-			memberJSON.put("first name", firstName);
-			memberJSON.put("last name", lastName);
-			memberJSON.put("books checked", booksJSON);
-
-			membersArray.put(memberJSON);
-
-			Files.write(UserHandler.getFilePath(), membersJSON.toString().getBytes(), StandardOpenOption.WRITE);
-		} catch (Exception e1) {
-			throw new IllegalArgumentException();
-		}
-	}
-	
-	private void getMemberJSON() {
-		try {
-			InputStream is = new FileInputStream(UserHandler.getFileName());
-			String memberFileText = IOUtils.toString(is, "UTF-8");
-			JSONObject membersJSON = new JSONObject(memberFileText);
-			JSONArray membersArray = membersJSON.getJSONArray("members");
-
-			for (int i = 0; i < membersArray.length(); i++) {
-
-				JSONObject mem = membersArray.getJSONObject(i);
-				String uname = mem.getString("username");
-				if (uname.equals(userName)) {
-					memberJSON = mem;
-					booksJSON = memberJSON.getJSONArray("books checked");
-				}
-			}
-			
-			System.out.println(membersJSON);
-		} catch (Exception e1) {
-		}
-		
-	}
-
-	public void checkout(String title, String author) {
-		JSONObject book = new JSONObject();
-		book.put("title", title);
-		book.put("author", author);
-		book.put("checkout date", LocalDate.now().toString());
-
-		handleBook(book, "checkout");
-	}
-
-	public void readBook(String title, String author) {
-		JSONObject book = new JSONObject();
-		book.put("title", title);
-		book.put("author", author);
-		book.put("checkout date", LocalDate.now().toString());
-
-		handleBook(book, "open");
-	}
-
-	public void returnBook(String title, String author) {
-
-		JSONObject book = new JSONObject();
-		book.put("title", title);
-		book.put("author", author);
-		book.put("checkout date", LocalDate.now().toString());
-
-		handleBook(book, "return");
-	}
-
-	public void handleBook(JSONObject book, String action) {
-		try {
-			InputStream is = new FileInputStream(UserHandler.getFileName());
-			String memberFileText = IOUtils.toString(is, "UTF-8");
-			JSONObject membersJSON = new JSONObject(memberFileText);
-			JSONArray membersArray = membersJSON.getJSONArray("members");
-
-			for (int i = 0; i < membersArray.length(); i++) {
-
-				memberJSON = membersArray.getJSONObject(i);
-				String uname = memberJSON.getString("username");
-				if (uname.equals(userName)) {
-					booksJSON = memberJSON.getJSONArray("books checked");
-
-					switch (action) {
-					case "checkout":
-						booksJSON.put(book);
-						break;
-					case "return":
-						if (hasBook(book) >= 0)
-							booksJSON.remove(hasBook(book));
-						break;
-					case "open":
-						if (hasBook(book) >= 0)
-							openBook(book.getString("title") + book.getString("author"));
-					}
-				}
-			}
-			Files.write(UserHandler.getFilePath(), membersJSON.toString().getBytes(), StandardOpenOption.WRITE);
-		} catch (Exception e1) {
-		}
-	}
-
-	private int hasBook(JSONObject book) {
-		for (int j = 0; j < booksJSON.length(); j++) {
-			JSONObject bookJSON = booksJSON.getJSONObject(j);
-			String title = bookJSON.getString("title");
-			String author = bookJSON.getString("author");
-			if (title.equals(book.getString("title")) && author.equals(book.getString("author"))) {
-				return j;
-			}
-		}
-		return -1;
-	}
-
-	@Override
-	String getName() {
-		return firstName + " " + lastName;
-	}
-
-	@Override
-	String getUserName() {
-		return userName;
-	}
-
-	@Override
-	String getPassword() {
-		return password;
-	}
-
-	@Override
-	public int compareTo(User o) {
-
-		if (o == this)
-			return 0;
-
-		int nameDiff = (o.getName().compareTo(this.getName()));
-		if (nameDiff > 0)
-			return 1;
-		else if (nameDiff < 0)
-			return -1;
-
-		int userNameDiff = (o.getUserName().compareTo(this.getUserName()));
-		if (userNameDiff > 0)
-			return 1;
-		else if (userNameDiff < 0)
-			return -1;
-
-		return 0;
-	}
-
-}
-
-class AdminUser extends User {
-
-	private final String ADMIN_USER = "ADMIN";
-	private final String ADMIN_PASS = "ADMIN";
-
-	public AdminUser() {
-		setUserName(ADMIN_USER);
-		setPassword(ADMIN_PASS);
-	}
-
-	@Override
-	String getName() {
-		return "Admin User";
-	}
-
-	@Override
-	String getUserName() {
-		return ADMIN_USER;
-	}
-
-	@Override
-	String getPassword() {
-		return ADMIN_PASS;
-	}
-
-	@Override
-	public int compareTo(User o) {
-		return 0;
-	}
+	public void openBook(String pdfName);
 
 }
