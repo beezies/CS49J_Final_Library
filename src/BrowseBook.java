30dc26039d8fedd7d1718189199ab3d86954b02e
Kelvin Huynh <131208924+pwner156@users.noreply.github.com>
2023-05-19 04:10:48 -0700

    Update BrowseBook.java

--- src/BrowseBook.java
+++ src/BrowseBook.java
@@ -18,13 +18,17 @@ public class BrowseBook extends JFrame implements ActionListener, HasTable {
 	String[] cols = { "Title", "Author", "Genre" };
 	String[][] bookData;
 	String[] selectedBook;
-
+/*
+ * displaying screen to pick from the book list
+ */
 	public BrowseBook(User u) {
 
 		setTitle("Browse Books");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(600, 500);
-
+/*
+ * creating new panel 
+ */
 		JPanel mainPanel = new JPanel();
 		SpringLayout layout = new SpringLayout();
 		mainPanel.setLayout(layout);
@@ -33,7 +37,9 @@ public class BrowseBook extends JFrame implements ActionListener, HasTable {
 		mainLabel.setFont(new Font("Serif", Font.PLAIN, 30));
 		layout.putConstraint(SpringLayout.WEST, mainLabel, 250, SpringLayout.WEST, mainPanel);
 		layout.putConstraint(SpringLayout.NORTH, mainLabel, 25, SpringLayout.NORTH, mainPanel);
-
+/*
+ * Creating a back button to go back to the previous screen
+ */
 		JButton back = new JButton("Back");
 		back.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -48,7 +54,9 @@ public class BrowseBook extends JFrame implements ActionListener, HasTable {
 		});
 		
 		String[][] data = getData();
-
+/*
+ * creating table to show books
+ */
 		JTable table = new JTable(data, cols);
 		table.setPreferredScrollableViewportSize(new Dimension(500, 250));
 		table.setFillsViewportHeight(true);
@@ -62,7 +70,9 @@ public class BrowseBook extends JFrame implements ActionListener, HasTable {
 		JScrollPane scrollPane = new JScrollPane(table);
 		layout.putConstraint(SpringLayout.WEST, scrollPane, 50, SpringLayout.WEST, mainPanel);
 		layout.putConstraint(SpringLayout.NORTH, scrollPane, 80, SpringLayout.NORTH, mainPanel);
-
+/*
+ * creating check out button
+ */
 		JButton checkout = new JButton("Check Out");
 		layout.putConstraint(SpringLayout.EAST, checkout, -10, SpringLayout.EAST, mainPanel);
 		layout.putConstraint(SpringLayout.SOUTH, checkout, -10, SpringLayout.SOUTH, mainPanel);
@@ -72,6 +82,9 @@ public class BrowseBook extends JFrame implements ActionListener, HasTable {
 
 		newCheckoutMsg.setVisible(false);
 		mainPanel.add(newCheckoutMsg);
+/*
+* When button is pressed user checks out book and it will display due date
+*/
 		checkout.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if (e.getSource() == checkout) {
