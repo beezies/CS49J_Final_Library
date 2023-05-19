44630d8f3c0889f2dd70112e062f9b65e0937515
Kelvin Huynh <131208924+pwner156@users.noreply.github.com>
2023-05-19 04:25:07 -0700

    Update User.java

--- src/User.java
+++ src/User.java
@@ -2,14 +2,23 @@ import java.awt.Desktop;
 import java.io.File;
 
 abstract class User implements Comparable<User>, CanOpenFile {
-
+/**
+* Quick constructor for users
+* 
+* @param userName
+* @param password
+*/
 	protected String userName;
 	protected String password;
-
+/*
+ * setter for user name
+ */
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
-
+/*
+* setter for password
+*/
 	public void setPassword(String password) {
 		this.password = password;
 	}
@@ -19,7 +28,9 @@ abstract class User implements Comparable<User>, CanOpenFile {
 	abstract String getUserName();
 
 	abstract String getPassword();
-
+/*
+ * saves information into file
+ */
 	@Override
 	public void openFile(String fileName) {
 		if (Desktop.isDesktopSupported()) {
@@ -37,7 +48,9 @@ interface CanOpenFile {
 	public void openFile(String pdfName);
 
 }
-
+/*
+ * admin login and password information
+ */
 class AdminUser extends User {
 
 	private final String ADMIN_USER = "ADMIN";
@@ -47,22 +60,30 @@ class AdminUser extends User {
 		setUserName(ADMIN_USER);
 		setPassword(ADMIN_PASS);
 	}
-
+/*
+ * getters for admin name
+ */
 	@Override
 	String getName() {
 		return "Admin User";
 	}
-
+/*
+* getters for admin user name
+*/
 	@Override
 	String getUserName() {
 		return ADMIN_USER;
 	}
-
+/*
+ * setters for admin password
+ */
 	@Override
 	String getPassword() {
 		return ADMIN_PASS;
 	}
-
+/*
+ * compares if information input is the same as the one stored
+ */
 	@Override
 	public int compareTo(User o) {
 		return 0;
