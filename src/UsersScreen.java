801b8aaf67e0c156733f25337aa5950f421abbf2
Kelvin Huynh <131208924+pwner156@users.noreply.github.com>
2023-05-19 04:32:17 -0700

    Update UsersScreen.java

--- src/UsersScreen.java
+++ src/UsersScreen.java
@@ -18,7 +18,9 @@ public class UsersScreen extends JFrame implements HasTable {
 	public static void main(String[] args) {
 		UsersScreen u = new UsersScreen();
 	}
-
+/*
+ * displaying all current users
+ */
 	public UsersScreen() {
 		setTitle("Viewing Users");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -27,12 +29,16 @@ public class UsersScreen extends JFrame implements HasTable {
 		JPanel mainPanel = new JPanel();
 		SpringLayout layout = new SpringLayout();
 		mainPanel.setLayout(layout);
-		
+/*
+* 	creating button to display all users panel
+*/		
 		JLabel mainLabel = new JLabel("All Users");
 		mainLabel.setFont(new Font("Serif", Font.PLAIN, 20));
 		layout.putConstraint(SpringLayout.WEST, mainLabel, 250, SpringLayout.WEST, mainPanel);
 		layout.putConstraint(SpringLayout.NORTH, mainLabel, 25, SpringLayout.NORTH, mainPanel);
-
+/*
+ * button to go back to previous screen
+ */
 		JButton back = new JButton("Back");
 		back.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -42,7 +48,9 @@ public class UsersScreen extends JFrame implements HasTable {
 		});		
 		
 		String[][] data = getData();
-		
+/*
+ * displaying current users into a table		
+ */		
 		JTable table = new JTable(data, cols);
 		table.setPreferredScrollableViewportSize(new Dimension(400, 200));
 		table.setFillsViewportHeight(true);
@@ -57,7 +65,9 @@ public class UsersScreen extends JFrame implements HasTable {
 		add(mainPanel);
 		setVisible(true);
 	}
-
+/*
+ * getter, obtains data from members
+ */
 	@Override
 	public String[][] getData() {
 		Member[] mems = UserHandler.getSortedMembersArray();
